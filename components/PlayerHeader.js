8f3f0209cd76850331607618be764f46a23a14f4
sachinpsgg <sachin@psglobalgroup.com>
2025-01-17 14:08:38 +0530

    initial commit

---
+++ components/PlayerHeader.js
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react';
+import {CameraIcon} from "lucide-react";
+
+const PlayerHeader = () => {
+    return (
+        <div className="flex justify-between items-center absolute w-full text-white p-2">
+            <h2 className="font-medium text-2xl ">Reels</h2>
+            <CameraIcon size={20} color="white"/>
+        </div>
+    );
+};
+
+export default PlayerHeader;
---
+++ config/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; // Import Firestore
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDFMfhCIy8M5gOwGEg4tVNFbR4mV9mPrXw",
+    authDomain: "insta-reels-clone-c41a9.firebaseapp.com",
+    projectId: "insta-reels-clone-c41a9",
+    storageBucket: "insta-reels-clone-c41a9.firebasestorage.app",
+    messagingSenderId: "712373936525",
+    appId: "1:712373936525:web:363f61c6a7c3ca69ac5dc4",
+    measurementId: "G-XTSXG2ZXSJ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+export { db };
